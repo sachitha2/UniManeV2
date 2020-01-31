c2405f43ef7be4f140292b14f7d637a4ff8e68b8
sachitha2 <shpsachitha@gmail.com>
2020-01-31 22:37:50 +0530

    Student ui dev

--- src/com/Uni/StudentProfileUI.java
+++ src/com/Uni/StudentProfileUI.java
@@ -5,28 +5,30 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class StudentUI {
+public class StudentProfileUI {
+    private JButton backToStudentButton;
+    private JTextField textField1;
+    private JTextField textField2;
     private JPanel panel1;
-    private JButton studentCoursesButton;
-    private JButton studentProfileButton;
+    private JPasswordField passwordField1;
+    private JPasswordField passwordField2;
+    private JButton saveButton;
 
-
-    StudentUI() {
-        JFrame frame = new JFrame("StudentUI");
+    StudentProfileUI() {
+        JFrame frame = new JFrame("StudentProfileUI");
         frame.setContentPane(this.panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
 
-
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         frame.setSize(screenSize.width,screenSize.height);
-        studentProfileButton.addActionListener(new ActionListener() {
+        backToStudentButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 frame.setVisible(false);
 
-                StudentProfileUI studentProfileUI = new StudentProfileUI();
+                StudentUI studentUI = new StudentUI();
             }
         });
     }
